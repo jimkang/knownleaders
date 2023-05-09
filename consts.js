3cdec3d9a201fba57f525eb8c3e85ce9c285a8ce
Jim Kang <jimkang@gmail.com>
2023-05-09 19:26:09 -0400

    Filter out entries in which the band name is the wikidata id.

--- consts.js
+++ consts.js
@@ -1,5 +1,5 @@
 // From https://www.ssa.gov/oact/babynames/decades/century.html
-module.exports = [
+var commonFirstNames = [
   'James',
   'Mary',
   'John',
@@ -201,3 +201,8 @@ module.exports = [
   'Logan',
   'Kayla'
 ];
+
+module.exports = {
+  commonFirstNames,
+  bandQueryURL: 'https://query.wikidata.org/sparql?query=SELECT%20%3Fgroup%20%3FgroupLabel%20WHERE%20%7B%0A%20%20%3Fgroup%20wdt%3AP31%20wd%3AQ2088357.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%7D%0A'
+};
--- tasks/get-leader-fact.js
+++ tasks/get-leader-fact.js
@@ -1,12 +1,14 @@
 var splitToWords = require('split-to-words');
-var commonFirstNames = require('../consts');
+var { commonFirstNames, bandQueryURL } = require('../consts');
 
-async function getLeaderFact({ probable, handleError, fetch }) {
+// var wikidataIdRegex = /Q\d+$/;
+
+async function getLeaderFact({ probable, handleError, fetch, maxTries = 100 }) {
   var wdObjects;
 
   try {
     wdObjects = await Promise.all([
-      getFromWikidata('https://query.wikidata.org/sparql?query=SELECT%20%3Fgroup%20%3FgroupLabel%20WHERE%20%7B%0A%20%20%3Fgroup%20wdt%3AP31%20wd%3AQ2088357.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%7D%0A'),
+      getFromWikidata(fetch, bandQueryURL),
       // getFromWikidata('https://query.wikidata.org/sparql?query=SELECT%20%3Fname%20%3FnameLabel%20WHERE%20%7B%0A%20%20%3Fname%20wdt%3AP31%20wd%3AQ202444.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%7D%0A')
     ]);
   } catch (error) {
@@ -15,9 +17,18 @@ async function getLeaderFact({ probable, handleError, fetch }) {
   var groupsObj = wdObjects[0];
   // var namesObj = wdObjects[1];
 
-  var selectedGroupObj = probable.pick(groupsObj?.results?.bindings || []);
-  const wikidataId = selectedGroupObj?.group?.value;
-  const groupName = selectedGroupObj?.groupLabel?.value;
+  var wikidataURL;
+  var groupName;
+  for (let i = 0; i < maxTries; ++i) {
+    let selectedGroupObj = probable.pick(groupsObj?.results?.bindings || []);
+    wikidataURL = selectedGroupObj?.group?.value;
+    groupName = selectedGroupObj?.groupLabel?.value;
+    if (!wikidataURL.includes(groupName)) {
+      break;
+    }
+    // Some query results put the wikidata id in the name slot. If we hit one of those, keep looking.
+  }
+
   const groupWords = splitToWords(groupName);
   // const selectedLeaderObj = probable.pick(namesObj?.results?.bindings || []);
   // const leaderGivenName = selectedLeaderObj?.nameLabel?.value;
@@ -25,21 +36,21 @@ async function getLeaderFact({ probable, handleError, fetch }) {
   const leaderName = leaderGivenName + ' ' + groupWords[groupWords.length - 1];
 
   return {
-    wikidataId,
+    wikidataURL,
     groupName,
     leaderName,
-    sentence: `We all know that the group ${groupName} was founded by ${leaderName}.`
+    sentence: `We all know that the group ${groupName} was founded by ${leaderName}.`,
   };
 }
 
-async function getFromWikidata(url) {
-  var res = await fetch(
-    url, { headers: { Accept: 'application/sparql-results+json' } }
-  );
+async function getFromWikidata(fetch, url) {
+  var res = await fetch(url, {
+    headers: { Accept: 'application/sparql-results+json' },
+  });
   if (!res.ok) {
     throw new Error(
       `Got ${res.status} while trying to find a group to talk about.`
-    )
+    );
   }
 
   var parsed = await res.json();
