323f980f4cdd7fd279a30351e1d2ddb2121f537f
Jim Kang <jimkang@gmail.com>
2023-05-09 12:51:04 -0400

    Wikidata request working.

--- app.js
+++ app.js
@@ -19,6 +19,21 @@ var prob;
     windowObject: window,
   });
   routeState.routeFromHash();
+
+  try {
+    var res = await fetch(
+      'https://query.wikidata.org/sparql?query=SELECT%20%3Fgroup%20%3FgroupLabel%20WHERE%20%7B%0A%20%20%3Fgroup%20wdt%3AP31%20wd%3AQ2088357.%0A%20%20SERVICE%20wikibase%3Alabel%20%7B%20bd%3AserviceParam%20wikibase%3Alanguage%20%22%5BAUTO_LANGUAGE%5D%2Cen%22.%20%7D%0A%7D%0A',
+      { headers: { 'Accept': 'application/sparql-results+json' } });
+    if (!res.ok) {
+      handleError(new Error(`Got ${res.status} while trying to find a group to talk about.`));
+      return;
+    }
+
+    var groupsObj = await res.json();
+    console.log(groupsObj);
+  } catch (error) {
+    handleError(error);
+  }
 })();
 
 async function followRoute({
