de0e2d2f83751867743743b5a42c99d21597f6a1
Jim Kang <jimkang@gmail.com>
2023-05-10 23:01:46 -0400

    Drop unused config.

--- rollup.config.js
+++ rollup.config.js
@@ -3,8 +3,6 @@
 import createConfig from './rollup-tools/base-config';
 import { serve } from './rollup-tools/config-tools';
 
-var auroraDrawrers = [];
-
 // Inspired by https://github.com/Tom-Siegel/multi-page-svelte/blob/5dd47f9ffe3cbddbaa5e29be5056ce1ed56060b2/rollup-pages.config.js#L45
 var configs = [
   {
@@ -15,15 +13,6 @@ var configs = [
     serveOpts: { port: 7000 },
   },
 ]
-  .concat(
-    auroraDrawrers.map((v) => ({
-      input: `knownleaderss/${v}/${v}-knownleaders.js`,
-      outputFile: `knownleaderss/${v}/${v}-knownleaders-bundle.js`,
-      reloadPath: `knownleaderss/${v}`,
-      serve: process.env.APP === v && serve,
-      serveOpts: { rootDir: '.', serveDir: `knownleaderss/${v}`, port: 6001 },
-    }))
-  )
   .map(createConfig);
 
 export default configs;
